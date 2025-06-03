bb4b3dc8fca7be33c547b340051f3eaf4d1d3815
Antônio Rocha <antoniojunior159@gmail.com>
2025-06-03 20:25:51 -0300

    Create a project

---
+++ MongoDBApplication/MongoAPI/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoAPI.Data;
+using MongoAPI.Model;
+using MongoDB.Driver;
+
+namespace MongoAPI.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly MongoDBContext _context;
+
+    public ProductsController(MongoDBContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Product>> Get()
+    {
+        return await _context.Products.Find(_ => true).ToListAsync();
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> Get(string id)
+    {
+        var product = await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> Create(Product product)
+    {
+        await _context.Products.InsertOneAsync(product);
+        return CreatedAtRoute(new { id = product.Id }, product);
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(string id, Product productIn)
+    {
+        var product = await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        await _context.Products.ReplaceOneAsync(p => p.Id == id, productIn);
+
+        return NoContent();
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var product = await _context.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        await _context.Products.DeleteOneAsync(p => p.Id == id);
+
+        return NoContent();
+    }
+}
---
+++ MongoDBApplication/MongoAPI/Data/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MongoAPI.Data;
+
+public class MongoDBSettings
+{
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; } 
+}
+
